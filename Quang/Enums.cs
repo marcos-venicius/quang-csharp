93d7acf109084979f87cf05d3a18156d1374161f
Marcos Venicius <marcosdevelopment.business@gmail.com>
2026-04-03 13:34:35 -0300

    fix(lexer): remove Enums.cs file

--- Quang/Enums.cs
+++ Quang/Enums.cs
@@ -1,27 +1,2 @@
 namespace Quang;
 
-internal enum TokenKind
-{
-	OpenParen,
-	CloseParen,
-	AndKeyword,
-	OrKeyword,
-	NilKeyword,
-	EqKeyword,
-	NeKeyword,
-	GtKeyword,
-	LtKeyword,
-	GteKeyword,
-	LteKeyword,
-	RegKeyword,
-
-	Symbol,
-
-	Integer,
-	Atom,
-	String,
-	Float,
-
-	TrueKeyword,
-	FalseKeyword,
-}
--- Quang/Token.cs
+++ Quang/Token.cs
@@ -14,3 +14,28 @@ internal class Token
     }
 }
 
+internal enum TokenKind
+{
+	OpenParen,
+	CloseParen,
+	AndKeyword,
+	OrKeyword,
+	NilKeyword,
+	EqKeyword,
+	NeKeyword,
+	GtKeyword,
+	LtKeyword,
+	GteKeyword,
+	LteKeyword,
+	RegKeyword,
+
+	Symbol,
+
+	Integer,
+	Atom,
+	String,
+	Float,
+
+	TrueKeyword,
+	FalseKeyword,
+}
